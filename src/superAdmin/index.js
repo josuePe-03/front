fb15d521a3e53640dcde55c8d2f95e525b4dd3fd
Josue Perez <josuepe03@hotmail.com>
2024-06-09 09:18:09 -0600

    UPDATE

--- src/superAdmin/index.js
+++ src/superAdmin/index.js
@@ -4,4 +4,8 @@ export {default as TecnicosSuperAdminPage} from "./pages/TecnicosSuperAdminPage"
 export {default as OperadoresSuperAdminPage} from "./pages/OperadoresSuperAdminPage"
 export {default as CompañiasSuperAdministradoresPage} from "./pages/CompañiasSuperAdministradoresPage"
 
+export {default as AgregarCentroMedico} from "./pages/CompañiaPages/AgregarCentroMedico"
+export {default as EditarCentroMedico} from "./pages/CompañiaPages/EditarCentroMedico"
+
+
 
